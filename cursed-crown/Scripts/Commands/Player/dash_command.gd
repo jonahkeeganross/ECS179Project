2bf304432f640ee6d461f95223a38013e9f3e07d
shavolkov <shavolkov@gmail.com>
2025-11-09 13:31:37 -0800

    not sure what im doing but we ball

---
+++ cursed-crown/Scripts/Commands/Player/dash_command.gd
@@ -0,0 +1,16 @@
+class_name DashCommand
+extends Command
+
+
+var _cooldown: float = 1.0
+var _dash_length: float = 80
+
+func execute(character: Character) -> Status:
+	#var input = character.movement_speed
+	if character.velocity.length() < 1:
+		return Status.DONE # In the case that player is not moving dont dash
+	var _cur_vel_norm = character.velocity.normalized()
+	character.position = character.position + _cur_vel_norm * _dash_length
+	#character.sprite.flip_h = false
+	#character.change_facing(Character.Facing.RIGHT)
+	return Status.DONE
